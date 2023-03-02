8068095f64b54e08729b7862a7ea556c9372b1a7
Wolfie <vlcek.rx@gmail.com>
2023-03-02 02:30:37 +0100

    BE - remove unused files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
 import { CurrencyRatesModule } from './currency-rates/currency-rates.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), CurrencyRatesModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
